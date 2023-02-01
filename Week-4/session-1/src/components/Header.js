9b2f3ece06ef7b6fcb39c4e0e0edc651f405dd71
Sri Sri <srisri.m@sapiens.com>
2023-02-01 11:12:07 +0530

    Add files via upload

--- Week-4/session-1/src/components/Header.js
+++ Week-4/session-1/src/components/Header.js
@@ -1,1 +1,29 @@
-test
+import { useState } from "react";
+
+const HeaderComponent = () => { 
+    const[isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+    <div className="header">
+        <Title />
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+        {isLoggedIn ? 
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>: 
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>}
+    </div>
+    )
+}
+
+export const Title = () => (
+    <div className="title">
+        <a href="#"><img alt="logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/cbbxaegl2u2fvrss6pec" /></a>
+    </div>
+)
+
+export default HeaderComponent;
